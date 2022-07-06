249a3abadf94d7f776c455bc1869cc4339e6da04
o.usenko <o.usenko@spd-ukraine.com>
2022-07-06 17:27:57 +0300

    add header

--- src/app/containers/main/main.module.ts
+++ src/app/containers/main/main.module.ts
@@ -6,9 +6,10 @@ import { MainRoutingModule } from 'src/app/containers/main/main-page.routing';
 import { MainComponent } from 'src/app/containers/main/main.component';
 import { VillageComponent } from 'src/app/containers/main/village/village.component';
 import { ResourcesComponent } from 'src/app/containers/main/resources/resources.component';
+import { HeaderComponent } from 'src/app/containers/main/header/header.component';
 
 @NgModule({
-  declarations: [MainComponent, VillageComponent, ResourcesComponent],
+  declarations: [MainComponent, VillageComponent, ResourcesComponent, HeaderComponent],
   imports: [CommonModule, RouterModule, MainRoutingModule]
 })
 export class MainModule {}
